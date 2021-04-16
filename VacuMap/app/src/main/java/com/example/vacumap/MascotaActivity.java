7ec8ea14328bf83850e2da1f07a438d44d0ad5e1
J Camilo Rpo V <12camilorpo@gmail.com>
2021-04-16 12:17:57 -0500

    Creación de proyecto

---
+++ VacuMap/app/src/main/java/com/example/vacumap/MascotaActivity.java
@@ -0,0 +1,14 @@
+package com.example.vacumap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MascotaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mascota);
+    }
+}
---
+++ VacuMap/app/src/main/java/com/example/vacumap/MascotaProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.vacumap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MascotaProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mascota_profile);
+    }
+}
