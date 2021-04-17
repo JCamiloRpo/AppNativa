dfd854af98901cacf409ee6ea90351075468165b
J Camilo Rpo V <12camilorpo@gmail.com>
2021-04-17 16:31:34 -0500

    Splash

---
+++ PetCare/app/src/main/java/com/example/vacumap/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.vacumap;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        TimerTask tarea = new TimerTask() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(), MascotaActivity.class);
+                startActivity(i);
+                finish();
+            }
+        };
+
+        Timer t = new Timer();
+        t.schedule(tarea, 5000);
+    }
+}
