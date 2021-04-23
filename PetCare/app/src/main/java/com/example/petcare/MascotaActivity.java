b3528e825f04a761939f6501406d2b2610f50f0e
J Camilo Rpo V <60258315+JCamiloRpo@users.noreply.github.com>
2021-04-23 08:21:38 -0500

    Boton Acerca de

--- PetCare/app/src/main/java/com/example/petcare/MascotaActivity.java
+++ PetCare/app/src/main/java/com/example/petcare/MascotaActivity.java
@@ -3,6 +3,8 @@ package com.example.petcare;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -21,6 +23,28 @@ public class MascotaActivity extends AppCompatActivity {
         initButtons();
         initList();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_info, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                this.finish();
+                return true;
+            case R.id.action_info:
+                /*Intent i = new Intent(getApplicationContext(), MascotaEditActivity.class);
+                startActivity(i);*/
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 
     private void initButtons(){
         btnAgregar = (Button) findViewById(R.id.BtnAddPet);
